74a434c778d229c13a8429b8f06e4f8642eda5da
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-08-20 21:41:21 +0500

    Add files via upload

---
+++ CheckArray/src/checkarray/CheckArray.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package checkarray;
+
+/**
+ *
+ * @author Ahsan Khalid
+ */
+public class CheckArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int arr[]=new int[6];
+        arr[0]=24;
+        arr[1]=56;
+        arr[3]=563;
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+        
+    }
+
+}
