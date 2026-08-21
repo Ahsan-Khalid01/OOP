0393704f546a877032fdb1f4b95586399bc70d27
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-08-21 21:44:08 +0500

    Add files via upload

---
+++ checkdatabase/src/checkdatabase/aBC.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package checkdatabase;
+
+/**
+ *
+ * @author dcui
+ */
+public class aBC {
+    private int id;
+    private String Name;
+    private String FName;
+    
+    
+}
